05c513e5456af2da39dab2f6c4e261042c03c8ad
kshaternik <kseniyashaternik@gmail.com>
2023-03-10 20:23:28 +0300

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,34 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class task2 {
+   
+    public static void main(String[] args) {
+        FourthTask fourthTask = new FourthTask();
+        fourthTask.writer();
+
+        class FourthTask {
+
+            public StringBuilder generateString() {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < 100; i++) {
+                    stringBuilder.append("TEST ");
+                }
+                return stringBuilder;
+            }
+
+            public void writer() {
+                try {
+                    FileOutputStream fileOutputStream = new FileOutputStream("test.txt");
+
+                    fileOutputStream.write(generateString().toString().getBytes());
+
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    System.out.println("Проблема вывода!");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
